04f2b2191b9a8d455bcb0c1a8bc1360c4541c997
Aryan <158206428+awasthere@users.noreply.github.com>
2024-09-19 23:32:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+const moon=document.getElementById('moon');
+const sun=document.getElementById('sun');
+const body=document.querySelector('body');
+const para=document.querySelector('p');
+const navlink=document.querySelectorAll('a');
+
+
+moon.addEventListener('click',()=>{
+    console.log("moon");
+    body.style.backgroundColor='black';
+    para.style.color='white';
+    document.querySelector('h2').style.color='white';
+    navlink.forEach((element)=>{
+        element.style.color="white";
+    })
+    sun.style.color="orange";
+    moon.style.color="gray";
+})
+
+sun.addEventListener('click',()=>{
+    console.log("moon");
+    body.style.backgroundColor='white';
+    para.style.color='black';
+    document.querySelector('h2').style.color='black';
+    navlink.forEach((element)=>{
+        element.style.color="black";
+    })
+    sun.style.color="gray";
+    moon.style.color="black";
+})
+
+
+
+
